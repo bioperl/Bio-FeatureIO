790a3fa8707db529a730f0e93c771128cc4c3a1b
Robert Buels <rmb32@cornell.edu>
2011-02-24 13:18:10 -0800

    offer parsed gff phase as both a first-class attr and as a tag value

--- lib/Bio/FeatureIO/gff.pm
+++ lib/Bio/FeatureIO/gff.pm
@@ -73,7 +73,13 @@ sub next_dataset {
                 (@feat{qw(-seq_id -source -primary_tag -start -end
                        -score -strand)}, (my $phase), $attstr) =
                     map {$_ ne '.' ? $_ : undef } split($GFF_SPLIT,$line);
-                $tags{phase} = defined $phase ? [$phase] : [];
+                if( defined $phase ) {
+                    $tags{phase} = [$phase];
+                    $feat{-phase} = $phase;
+                } else {
+                    $tags{phase} = [];
+                }
+
                 for my $kv (split(/\s*;\s*/, $attstr)) {
                     my ($key, $rest) = split("$ATTRIBUTE_SPLIT", $kv, 2);
                     my @vals = map { $ATTRIBUTE_CONVERT->($_) } split(',',$rest);
